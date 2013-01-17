54a0772a9bb13ffe49236cffe8e187de4386ac7f
Mark Hindess <soft-github@temporalanomaly.com>
2013-01-17 22:20:24 +0000

    Build results of 70c0db2 (on master)

--- lib/AnyEvent/SerialPort.pm
+++ lib/AnyEvent/SerialPort.pm
@@ -108,6 +108,49 @@ This module is a subclass of L<AnyEvent::Handle> for serial ports.
 B<IMPORTANT:> This is a new API and is still subject to change.  Feedback
 and suggestions would be very welcome.
 
+=head1 METHODS
+
+=head2 C<new(%parameters)>
+
+Constructs an AnyEvent::SerialPort object based on the given
+parameters.  The permitted parameters are those of the
+L<AnyEvent::Handle> super class constructor (except C<connect> and
+C<fh> which would not make sense) and a C<serial_port> parameter.
+The value of the serial port parameter may be either:
+
+=over
+
+=item * the full path name of the serial device, or
+
+=item * a list reference containing the full path name of the serial
+        device and zero or more list references each containing a
+        L<Device::SerialPort> method name and arguments to be executed
+        on the wrapped L<Device::SerialPort> object.
+
+=back
+
+By default, the following setter methods are called on the underlying
+L<Device::SerialPort> object:
+
+=over
+
+=item C<baudrate(9600)>
+
+=item C<databits(8)>
+
+=item C<parity('none')>
+
+=item C<stopbits(1)>
+
+=item C<datatype('raw')>
+
+=back
+
+The second form of the C<serial_port> parameter value described above
+can be used to override these settings and apply additional setters.
+
+See the L</"SYNOPSIS"> for examples.
+
 =head2 C<serial_port()>
 
 Return the wrapped L<Device::SerialPort> object.
